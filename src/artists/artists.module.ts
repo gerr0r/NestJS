5bebf51b81bcda3039f290bc1c41bd6408c9edec
Gergan Ruschev <gergan.ruschev@yara.com>
2021-10-12 17:36:36 +0300

    feat: added empty repository for artist entity

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArtistsController } from './artists.controller';
+import { ArtistsRepository } from './artists.repository';
 import { ArtistsService } from './artists.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArtistsRepository])],
   controllers: [ArtistsController],
   providers: [ArtistsService]
 })
