524b9f7ef30f74be373e5b653ee77e3ee02c3dd4
Gergan Ruschev <gergan.ruschev@yara.com>
2021-10-12 17:20:13 +0300

    feat: added database configuration through env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ArtistsModule } from './artists/artists.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ArtistsModule]
+  imports: [
+    ArtistsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        database: config.get('DB_NAME'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    })
+  ]
 })
 export class AppModule {}
